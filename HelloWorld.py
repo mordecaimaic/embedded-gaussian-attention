457ee1d3a4869838ed434ef3db3e15b42d003847
mordecaimaic <110681224+mordecaimaic@users.noreply.github.com>
2025-05-30 14:34:42 +0800

    Remove redundant files

--- HelloWorld.py
+++
@@ -1,7 +0,0 @@
-import torch, torchaudio
-print(torch.__version__)           # 应是 2.5.1
-print(torch.version.cuda)          # '12.4'
-print(torch.cuda.is_available())   # True
-x = torch.rand(1, device='cuda')
-print(x)
-print("Hello World")
